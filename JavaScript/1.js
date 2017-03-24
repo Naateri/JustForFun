594524f36bf1f339597d1608d96ba63fd36f64ed
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-03-24 09:38:13 -0500

    js

---
+++ JavaScript/1.js
@@ -0,0 +1,19 @@
+//Si tu numero es menor a 70 imprime el numero si no te pide que lo metas de nuevo
+
+function askNumber(){
+	var num;
+	num = prompt("Ingrese un numero: ");
+	return num;
+}
+
+function main(){
+	var num = askNumber();
+	if (num < 70){
+		console.log("Ingrese otra vez.");
+		main();
+	} else {
+		console.log("Lo lograste!");
+	}
+}
+
+main()
